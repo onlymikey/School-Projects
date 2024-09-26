8401b27c87d8fa0c45c1d97becc00ae5b4874707
Mikey <72430453+onlymikey@users.noreply.github.com>
2024-09-26 13:54:05 -0600

    Delete SoftEng/taller_mecanico directory

--- SoftEng/taller_mecanico/Controllers/user_controller.py
+++
@@ -1,9 +0,0 @@
-from Services.user_service import UserService
-
-class UserController:
-    def __init__(self):
-        self.user_service = UserService()
-
-    def verify_credentials(self, username, password):
-        """Llamar al servicio para verificar si las credenciales son correctas."""
-        return self.user_service.verify_credentials(username, password)
--- SoftEng/taller_mecanico/Models/user_model.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, id, name, username, password, profile):
-        self.id = id
-        self.username = username
-        self.password = password
-        self.name = name
-        self.profile = profile
