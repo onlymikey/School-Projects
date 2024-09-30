f9675957e1b01a56535976548db2f194a7f8d805
onlymikey <angel.lopezb@proton.me>
2024-09-30 00:13:03 -0600

    Created vehicle_model.py

---
+++ SoftEng/taller_mecanico/Models/vehicle_model.py
@@ -0,0 +1,6 @@
+class Vehicle:
+    def __init__(self, registration, clientid, brand, model):
+        self.registration = registration
+        self.clientid = clientid
+        self.brand = brand
+        self.model = model
