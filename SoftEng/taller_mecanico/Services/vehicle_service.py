a3fabbe2c9aba27b5d9f139b97519d59204b87e5
onlymikey <angel.lopezb@proton.me>
2024-10-03 17:09:23 -0600

    Added a function to check if  vehicle registration already exists (yeah you know what I did)

--- SoftEng/taller_mecanico/Services/vehicle_service.py
+++ SoftEng/taller_mecanico/Services/vehicle_service.py
@@ -8,6 +8,8 @@ class VehicleService:
 
     def create_vehicle(self, vehicle: Vehicle):
         # Crear un nuevo vehículo
+        if self.is_registration_exists(vehicle.registration):
+            raise ValueError("Ya existe un vehiculo con esta matricula.")
         self.vehicle_dao.create_vehicle(vehicle)
 
     def get_vehicle_by_registration(self, registration: str) -> Vehicle:
@@ -17,4 +19,9 @@ class VehicleService:
     def update_vehicle(self, vehicle: Vehicle):
         # Actualizar la información de un vehículo
         self.vehicle_dao.update_vehicle(vehicle)
+
+    def is_registration_exists(self, registration: str) -> bool:
+        """Check if a vehicle with the given registration already exists."""
+        vehicle = self.vehicle_dao.get_vehicle_by_registration(registration)
+        return vehicle is not None
 
