a5b7200d52f69584e571a4848f421e55355c1787
onlymikey <angel.lopezb@proton.me>
2024-10-03 03:07:06 -0600

    Created repair_model.py

---
+++ SoftEng/taller_mecanico/Models/repair_model.py
@@ -0,0 +1,9 @@
+class Repair:
+    def __init__(self, id_repair, matricula, id_part, in_date, out_date, quantity, problem):
+        self.id_repair = id_repair
+        self.matricula = matricula
+        self.id_part = id_part
+        self.in_date = in_date
+        self.out_date = out_date
+        self.quantity = quantity
+        self.problem = problem
