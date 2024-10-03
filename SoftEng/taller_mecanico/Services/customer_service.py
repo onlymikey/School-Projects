e6788e651310c4aad0e12671687cc6ec8b177499
onlymikey <angel.lopezb@proton.me>
2024-10-03 17:04:39 -0600

    Added validations for checking if phone number already exists (and fucking up Controller - View model in the process)

--- SoftEng/taller_mecanico/Services/customer_service.py
+++ SoftEng/taller_mecanico/Services/customer_service.py
@@ -8,7 +8,14 @@ class CustomerService:
 
     def create_customer(self, customer: Customer):
         # Insertar el cliente en la base de datos
+        if self.is_phone_number_exists(customer.phone):
+            raise ValueError("Ya existe un cliente con este numero de telefono.")
         self.customer_dao.save_customer(customer)
+
+    def is_phone_number_exists(self, phone_number: str) -> bool:
+        """Check if a customer with the given phone number already exists."""
+        customer = self.customer_dao.get_customer_by_phone_number(phone_number)
+        return customer is not None
 
     def get_customer(self, customer_id):
         # Obtener el cliente desde la base de datos
